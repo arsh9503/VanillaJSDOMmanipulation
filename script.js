37e70381801ecd6e7ff1973936f430d0ac546622
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-01 17:11:51 -0600

    Refactor fetchData function to handle nested data and improve code readability

--- script.js
+++ script.js
@@ -1,34 +1,56 @@
 async function fetchData(url) {
-    const response = await fetch(url);
-    (await response.json()).data.map((data) => {
-        console.log(data)
-        const element = {
-            tag: "div",
-            children: {
-                "h1": data.first_name + " " + data.last_name,
-                "h3": data.email,
-                "img": data.avatar
+   const response = await fetch(url);
+    Object.values((await response.json())).forEach(value => {
+        if(typeof value === "object"){
+            value.map((data) => {
+                const element = {
+                    tag: "div",
+                    children: {}
+                }
+                for (const [key, value] of Object.entries(data)) {
+                    if (typeof value === "object"){
+                        continue;
+                    }
+                    else if (typeof value === "string") { 
+                        if(value === ""){
+                            continue
+                        }
+                        else if(key.toLowerCase().match('im') || value.toLowerCase().match('im')){
+                            element.children.img = value;
+                        }
+                        else if (value.length < 20 || value.match("name")) {
+                            element.children.h1 = value
+                        }
+                        else {
+                            element.children.p = value
+                        }
+                       
+                     }
+                     else {
+                        element.children.h3 = value;
+                     }
+                }
+                const placeholder = document.createElement(element.tag);
+                placeholder.id = "data";
+                for (const [key, value] of Object.entries(element.children)) {
+                    const temp = document.createElement(key);
+                    if (key === "img") {
+                        temp.src = value
+                    }
+                    else {
+                        temp.innerHTML = value;
+                    }
+                    placeholder.appendChild(temp);
+                }
+                document.body.appendChild(placeholder)
             }
+        
+            )
         }
-        const placeholder = document.createElement(element.tag);
-        placeholder.id = "data";
-        for (const [key, value] of Object.entries(element.children)) {
-            const temp = document.createElement(key);
-            if (key === "img") {
-                temp.src = value
-            }
-            else {
-                temp.innerHTML = value;
-            }
-            placeholder.appendChild(temp);
-        }
-        document.body.appendChild(placeholder)
-    }
-
-    )
+    } )
 }
 
-fetchData("https://reqres.in/api/users");
+fetchData("https://newsapi.org/v2/everything?q=tesla&from=2024-07-01&sortBy=publishedAt&apiKey=777ca7dc161f412fa30e6189aac0ddb9");
 
 let currentIndex = 1
 
@@ -40,7 +62,6 @@ function changePage(direction) {
     if (document.getElementById("data")) {
         document.body.removeChild(document.getElementById("data"));
     }
-    console.log(currentIndex)
     currentIndex += direction;
     if (currentIndex < 1) {
         currentIndex = 1; // Ensure currentIndex doesn't go below 1
